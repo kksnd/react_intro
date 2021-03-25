00adc33949892c0f0c5be593f2dcca7e0a722053
koki.senda <koki.senda@rakuten.com>
2021-03-25 11:20:15 +0900

    Revert "create Counter in top page"
    This reverts commit f85a0ae8142156effe42fcc88d3b299f21e7ac0a.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
-        <Counter/ >
-
-        <br />
-
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
